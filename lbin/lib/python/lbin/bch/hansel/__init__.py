49013fea888c6445a4a3d61c3686cb48a8e643da
Bryan Hann <nobody@nowhere>
2021-10-17 11:34:52 -0230

    Change output of [bch.hansel.*].
    Include the [.bch] at the end of each path printed.

--- lbin/lib/python/lbin/bch/hansel/__init__.py
+++ lbin/lib/python/lbin/bch/hansel/__init__.py
@@ -2,8 +2,8 @@
 
 def gather(path):
     bch=path/'.bch'
-    if (path/'.bch').exists():
-        yield path
+    if bch.exists():
+        yield bch #path
         for child in path.glob('*'):
             yield from gather(child)
 
