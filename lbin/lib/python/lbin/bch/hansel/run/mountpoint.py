e081aa6e2eb2dbd8e5590cd6310901b05bf64095
Bryan Hann <nobody@nowhere>
2021-10-18 10:29:08 -0230

    Add [__builtin__].

--- lbin/lib/python/lbin/bch/hansel/run/mountpoint.py
+++ lbin/lib/python/lbin/bch/hansel/run/mountpoint.py
@@ -1,9 +1,8 @@
 import sys
 import importlib
+from lbin.bch.__bch__ import BCH
 
-from lbin.bch.builtin import BCH as bch
-
-BCH.fpath = __file__
+BCH._register_file(__file__)
 
 USAGE=f""" USAGE:\n\t{BCH.fname}\nDESCRIPTION:\n\tfoo\n"""
 
@@ -11,7 +10,6 @@ def main():
     if len(sys.argv)>1:
         sys.stderr.write(USAGE)
     else:
-        foo = importlib.import_module(BCH.bname)
-        print(foo)
+        print(BCH.dimport('.config').mountpoint())
 
 main()
